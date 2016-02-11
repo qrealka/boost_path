48873c73f6d1829d8fed8c7bfcc2bad5103d8299
Dmitry Loginov <Dmitry.Loginov@gonitro.com>
2016-02-11 22:01:37 +0300

    migrate from Hunter to Conan

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <boost/locale.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/fstream.hpp>
 
-using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+	std::cout << "Create and install global locale..." << std::endl;
+
+	std::locale::global(boost::locale::generator().generate(""));
+	std::cout << "Make boost FS use it..." << std::endl;
+	boost::filesystem::path::imbue(std::locale());
+
+    boost::filesystem::ofstream hello("пример.txt");
+    hello << "привет! שלום";
+	std::cout << "Text file created" << std::endl;
     return 0;
 }
