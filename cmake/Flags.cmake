daa1ba47a043c3ee4709432b14fb1c9aa261843a
Dmitry Loginov <Dmitry.Loginov@gonitro.com>
2016-02-20 14:41:55 +0300

    encoding CPP file w/o BOM and convert WCHAR consts to  utf8 code

--- cmake/Flags.cmake
+++ cmake/Flags.cmake
@@ -19,6 +19,6 @@ if(CMAKE_CXX_COMPILER_ID MATCHES "^(Apple)?Clang$")
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LibCXX} ${LibCXXAbi} -lpthread -ldl")
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   # Workaround for GCC bug https://bugs.launchpad.net/ubuntu/+source/gcc-defaults/+bug/1228201
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--no-as-needed -pthread")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--no-as-needed")
 endif()
 
--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include <fstream>
+#include <locale>
+
+#if _MSC_VER == 1900
+#include <windows.h>
+#include <locale.h>
+#include <filesystem>
+#else
 #include <boost/locale.hpp>
-#include <boost/system/error_code.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/fstream.hpp>
+#include <boost/container/string.hpp>
+#endif
 
 
 int main() {
+	std::ios_base::sync_with_stdio(false);
 	std::cout << "Create and install global locale..." << std::endl;
-	boost::system::error_code e;
+	std::wstring name(L"empty");
 
-	//std::locale l;
+#if _MSC_VER == 1900
+	std::experimental::filesystem::path p(u"пример.txt"); // 
+	std::ofstream hello(p);
+	hello.imbue(std::locale());
+#else
 	std::locale::global(boost::locale::generator().generate(""));
 	std::cout << "Make boost FS use it..." << std::endl;
 	boost::filesystem::path::imbue(std::locale());
-
-	//std::ofstream hello("пример.txt");
-	//hello.imbue(std::locale());
-    boost::filesystem::ofstream hello("пример.txt");
-    hello << "привет! שלום";
-	std::cout << "Text file created" << std::endl;
+	boost::filesystem::path p("пример.txt"); 
+    name = p.wstring();
+	std::wstring  tmp(L"\u0442\u0435\u0441\u0442");
+	boost::filesystem::ofstream hello(p); // can be use by pass "пример.txt" (implicity ctor boost::path)
+	hello.imbue(std::locale());
+	hello << name;
+	hello << tmp;
+	name = L"\n \u0441\u0442\u0440\u043e\u043a\u0430 ";
+	hello << name;
+#endif
+    hello << std::endl << "привет! שלום " << std::endl; // привет! שלום 
+	std::cout << "Text file created" << std::endl << "привет! שלום ";
     return 0;
 }
